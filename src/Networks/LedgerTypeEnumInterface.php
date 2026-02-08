85f1fca3bf3dd3129670b1acf6868e74085a3729
Furqan Siddiqui <hello@furqansiddiqui.com>
2026-02-08 17:32:02 +0400

    Add `LedgerTypeEnumInterface` and `LedgerType` enum to define ledger types

---
+++ src/Networks/LedgerTypeEnumInterface.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * Part of the "furqansiddiqui/blockchain-core-php" package.
+ * @link https://github.com/furqansiddiqui/blockchain-core-php
+ */
+
+declare(strict_types=1);
+
+namespace FurqanSiddiqui\Blockchain\Core\Networks;
+
+/**
+ * Describes a contract for enumerations related to ledger types.
+ * This interface extends PHP's native UnitEnum and is intended to define
+ * a set of constants or methods representing ledger type categories.
+ */
+interface LedgerTypeEnumInterface extends \UnitEnum
+{
+}
