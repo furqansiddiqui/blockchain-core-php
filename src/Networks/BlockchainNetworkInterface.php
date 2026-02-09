3180521c8defe66a69297cf69e01c6541b5b6dd5
Furqan Siddiqui <hello@furqansiddiqui.com>
2026-02-09 23:17:30 +0400

    Add `BlockchainNetworkInterface` as a marker for blockchain network implementations

---
+++ src/Networks/BlockchainNetworkInterface.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Part of the "furqansiddiqui/blockchain-core-php" package.
+ * @link https://github.com/furqansiddiqui/blockchain-core-php
+ */
+
+declare(strict_types=1);
+
+namespace FurqanSiddiqui\Blockchain\Core\Networks;
+
+/**
+ * Marker interface for a Blockchain Network.
+ */
+interface BlockchainNetworkInterface
+{
+}
